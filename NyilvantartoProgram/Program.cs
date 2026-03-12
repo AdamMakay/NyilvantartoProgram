b158c09ae15555210b2266403386ae182c317c84
Dominik Vékony <dominikvekony11@gmail.com>
2026-03-12 10:30:25 +0100

    Hozzáadva: dolgózó törlése

--- NyilvantartoProgram/Program.cs
+++ NyilvantartoProgram/Program.cs
@@ -5,34 +5,54 @@ namespace NyilvantartoProgram
     {
         static void Main(string[] args)
         {
-         
+
             adatok BeirtAdatok = null;
-            if(!File.Exists("adatok.txt"))
+            if (!File.Exists("adatok.txt"))
             {
-                File.WriteAllText("adatok.txt", "Nev;Kor;Cim;SzolgalatiIdo;Szakma;Fizetes\n");
+                File.WriteAllText("adatok.txt", "Név;Kor;Cím;SzolgalatiIdő;Szakma;Fizetés\n");
             }
             StreamWriter sw;
-            Console.Write("1 - Adatok megadása\n");
+            Console.Write("1 - Új dolgozó felvétele\n");
+            Console.Write("2 - Dolgozók listázása\n");
+            Console.Write("3 - Statisztikák\n");
+            Console.Write("4 - Dolgozó törlése\n");
             string valasz = Console.ReadLine();
-            if(valasz == "1")
+            if (valasz == "1")
             {
                 BeirtAdatok = EllVaroAdatok();
-                sw= new StreamWriter("adatok.txt", true);
+                sw = new StreamWriter("adatok.txt", true);
                 sw.WriteLine($"{BeirtAdatok.Nev};{BeirtAdatok.Kor};{BeirtAdatok.Cim};{BeirtAdatok.SzolgalatiIdo};{BeirtAdatok.Szakma};{BeirtAdatok.Fizetes}");
                 sw.Close();
             }
             if (valasz == "2")
             {
                 Console.WriteLine(File.ReadAllText("adatok.txt"));
-                
+
             }
             if (valasz == "3")
             {
-                Console.Write("nev: ");
+                Console.Write("Adja meg a lekérdezendő dolgozó nevét: ");
                 string adatok = Statisztika(Console.ReadLine());
                 Console.WriteLine(adatok);
             }
+            if (valasz == "4")
+            {
+                Console.Write("Mit szeretne csinálni?\n");
+                Console.Write("1 - Egy dolgozó törlése\n");
+                Console.Write("2 - Összes dolgozó törlése\n");
+
+                string torlesValasz = Console.ReadLine();
+
+                if (torlesValasz == "1")
+                {
+                    Console.Write("Adja meg a törlendő dolgozó nevét: ");
+                    string nev = Console.ReadLine();
+                    DolgozoTorles(nev);
+                }
+            }
         }
+
+
 
         static string Statisztika(string nev)
         {
@@ -52,14 +72,14 @@ namespace NyilvantartoProgram
 
         static adatok EllVaroAdatok()
         {
-            
+
 
             Console.Write("Adja meg a dolgozó nevét: ");
             string nev = Console.ReadLine();
 
-            
 
-            
+
+
 
             Console.Write("Adja meg a dolgozó életkorát: ");
             string kor = Console.ReadLine();
@@ -97,7 +117,26 @@ namespace NyilvantartoProgram
                 fizetes = Console.ReadLine();
             }
 
-            return new adatok(nev,kor,cim,szolgalatiIdo,szakma,fizetes);
+            return new adatok(nev, kor, cim, szolgalatiIdo, szakma, fizetes);
+        }
+        static void DolgozoTorles(string nev)
+        {
+            List<string> sorok = new List<string>(File.ReadAllLines("adatok.txt"));
+            List<string> ujSorok = new List<string>();
+
+            foreach (string sor in sorok)
+            {
+                string[] adatok = sor.Split(';');
+
+                if (adatok[0] != nev || adatok[0] == "Név")
+                {
+                    ujSorok.Add(sor);
+                }
+            }
+
+            File.WriteAllLines("adatok.txt", ujSorok);
+
+            Console.WriteLine("Dolgozó törölve.");
         }
     }
 }
